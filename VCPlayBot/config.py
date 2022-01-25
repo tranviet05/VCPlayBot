fe549c4c534b20ed4ad3b3c00255c773be093793
tranviet05 <92138026+tranviet05@users.noreply.github.com>
2022-01-25 10:50:30 +0700

    Update config.py

--- VCPlayBot/config.py
+++ VCPlayBot/config.py
@@ -7,24 +7,24 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 que = {}
-SESSION_NAME = getenv(BQApnomLhVuD6hOTzGOV-CFhJIOF7mYJYbWXLMpVZ9p16L1JtEfx1GPnkP3Z_A-ZOKS58TXQPEHlUgeluyNOCQ3xa0_kt83H3U_IFCNqLCwkx2PEL78Wkx9Iwwm8yHwVd_vfZk10hLi7283D3pe5eezSUGatASa25aD0Z127_-59Az4RqCAHIFn7DFZTv0M042YJueKGStKlTHyTnFJWH6Lx50CHADmPMdOO4D20TZeq72Deej1LUM1TMhOEJRltSNn4CPqJeerthDEG8BrwISwWqun-B6LTyh7edD32-6UA85RHCLRzQoQ0KqWtpa5pwylDcLHL5lBO3Y7zOCoDK5arbKJHRwA)
-BOT_TOKEN = getenv(5285501576:AAFq_V6osrcR3CvES8UJPlk-ri7nrtvmRdM)
-BOT_NAME = getenv(B-Airdrop Musical)
-UPDATES_CHANNEL = getenv(LaylaList)
-BG_IMAGE = getenv(https://telegra.ph/file/9b13ea3ce046a1a5c8098.png)
+SESSION_NAME = "BQApnomLhVuD6hOTzGOV-CFhJIOF7mYJYbWXLMpVZ9p16L1JtEfx1GPnkP3Z_A-ZOKS58TXQPEHlUgeluyNOCQ3xa0_kt83H3U_IFCNqLCwkx2PEL78Wkx9Iwwm8yHwVd_vfZk10hLi7283D3pe5eezSUGatASa25aD0Z127_-59Az4RqCAHIFn7DFZTv0M042YJueKGStKlTHyTnFJWH6Lx50CHADmPMdOO4D20TZeq72Deej1LUM1TMhOEJRltSNn4CPqJeerthDEG8BrwISwWqun-B6LTyh7edD32-6UA85RHCLRzQoQ0KqWtpa5pwylDcLHL5lBO3Y7zOCoDK5arbKJHRwA"
+BOT_TOKEN = "5285501576:AAFq_V6osrcR3CvES8UJPlk-ri7nrtvmRdM"
+BOT_NAME = "B-Airdrop Musical"
+UPDATES_CHANNEL = "LaylaList"
+BG_IMAGE = "https://telegra.ph/file/9b13ea3ce046a1a5c8098.png"
 admins = {}
-API_ID = int(getenv(17721070))
-API_HASH = getenv(4bba50e58a9cdf2662fad1c8b6fb663c)
-BOT_USERNAME = getenv(b_airdropbot)
-ASSISTANT_NAME = getenv(VCPlayAssistant)
-OWNER_NAME = getenv(Taobaorich)
-SUPPORT_GROUP = getenv(AwesomeSupport)
-PROJECT_NAME = getenv(VCPlayBot2.0)
-SOURCE_CODE = getenv(github.com/tranviet05/VCPlayBot)
-DURATION_LIMIT = int(getenv(7))
-ARQ_API_KEY = getenv(BIOFMG-JYQCKM-RXMUYK-PBMIIS-ARQ)
-PMPERMIT = getenv(PMPERMIT)
-LOG_GRP = getenv(1001744882169)
+API_ID = "17721070"
+API_HASH = "4bba50e58a9cdf2662fad1c8b6fb663c"
+BOT_USERNAME = "b_airdropbot"
+ASSISTANT_NAME = "VCPlayAssistant"
+OWNER_NAME = "Taobaorich"
+SUPPORT_GROUP = "AwesomeSupport"
+PROJECT_NAME = "VCPlayBot2.0"
+SOURCE_CODE = "github.com/tranviet05/VCPlayBot"
+DURATION_LIMIT = "7"
+ARQ_API_KEY = "BIOFMG-JYQCKM-RXMUYK-PBMIIS-ARQ"
+PMPERMIT = "PMPERMIT"
+LOG_GRP = "1001744882169"
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv(1822574407).split()))
+SUDO_USERS = "1822574407"
